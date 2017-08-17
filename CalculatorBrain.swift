b1225d1f100d68fc04c1d28f7f1b4dbf2d6926fe
Frank_Cerny <frankk.cerny@gmail.com>
2017-08-17 19:20:31 -0400

    Changed accumulator to a tuple value to encapsulate description as well, phased out description and updated comments in preparation for assinment 2 and more cleaning of the code.

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -15,25 +15,18 @@ struct CalculatorBrain {
      The accumulator is exactly like display value, except that it keeps a running total of what is on the screen, while displayValue is simply a computed property
     */
     
-    private var accumulator: Double?  // Use Optional Defaulting here maybe??
+    private var accumulator: (currentValue: Double,description: String)?
     private var resultIsPending = false
-    
-    // This is a test variable tuple that accumulator will become soon
-    
-    // private var testAccumulator: (currentNum: Double?, history: String?)
-    
-    // Keeps track of current operands and operators causing the current result (if result is pending) or the final result
-    
-    private var description = " "
     private var historyStringSuffix = " "
 
     
     // The enum allows us to create special data types that contain optional values that can be easily accessed
+    // Will also be used to piece together the description by using a second optional value which takes care of the description part
     
     private enum Operation {
-        case constant(Double)
-        case unaryOperation((Double) -> Double)
-        case binaryOperation((Double,Double) -> Double)
+        case constant(Double, String)
+        case unaryOperation((Double) -> Double, (String) -> String)
+        case binaryOperation((Double,Double) -> Double, (String,String) -> String)
         case equals
         case clear
         case random
@@ -45,21 +38,21 @@ struct CalculatorBrain {
     
     private var operations: Dictionary<String,Operation> = [
         // Option p inserts pi fyi
-        "π": Operation.constant(Double.pi),
-        "e": Operation.constant(M_E),
-        "√": Operation.unaryOperation(sqrt),
-        "＋": Operation.binaryOperation({ $0 + $1 }),
-        "−": Operation.binaryOperation({ $0 - $1 }),
-        "÷": Operation.binaryOperation({ $0 / $1 }),
-        "×": Operation.binaryOperation({ $0 * $1 }),
-        "±": Operation.unaryOperation({ -$0 }),
-        "cos": Operation.unaryOperation(cos),
-        "sin": Operation.unaryOperation(sin),
-        "tan": Operation.unaryOperation(tan),
+        "π": Operation.constant(Double.pi,"π"),
+        "e": Operation.constant(M_E, "e"),
+        "√": Operation.unaryOperation(sqrt,{"√(" + $0 + ")"}),
+        "＋": Operation.binaryOperation({ $0 + $1 }, {$0 + "+" + $1}),
+        "−": Operation.binaryOperation({ $0 - $1 }, {$0 + "-" + $1}),
+        "÷": Operation.binaryOperation({ $0 / $1 }, {$0 + "/" + $1}),
+        "×": Operation.binaryOperation({ $0 * $1 }, {$0 + "*" + $1}),
+        "±": Operation.unaryOperation({ -$0 }, {"±(" + $0 + ")"}),
+        "cos": Operation.unaryOperation(cos, {"cos(" + $0 + ")"}),
+        "sin": Operation.unaryOperation(sin, {"sin(" + $0 + ")"}),
+        "tan": Operation.unaryOperation(tan, {"tan(" + $0 + ")"}),
         "=": Operation.equals,
-         "∛": Operation.unaryOperation({pow( $0, 1/3 )}),
-         "ln": Operation.unaryOperation({ log2($0) }),
-         "^": Operation.binaryOperation({pow($0, $1)}),
+        "∛": Operation.unaryOperation({pow( $0, 1/3 )}, {"∛(" + $0 + ")"}),
+        "ln": Operation.unaryOperation({ log2($0) }, {"ln(" + $0 + ")"}),
+        "^": Operation.binaryOperation({pow($0, $1)}, {$0 + "^" + $1}),
          "rand": Operation.random,
          "c": Operation.clear
     ]
@@ -71,39 +64,30 @@ struct CalculatorBrain {
     mutating func performOperation(_ symbol: String) {
         if let operation = operations[symbol] {
             switch operation {
-            case .constant(let value):
-                description += symbol
-                accumulator = value
-                // testAccumulator = (value, testAccumulator.1! + symbol)
-                // print("History String: " + testAccumulator.1!)
-            case .unaryOperation(let function):
-                if resultIsPending {
-                    description += symbol + "(" + formatNumber(number: accumulator!) + ")"
-                }
-                else {
-                    description = symbol + "(" + description + ")"
-                }
-                    accumulator = function(accumulator!)
+            case .constant(let value, let symbol):
+                // description += symbol
+                accumulator = (value, symbol)
+            case .unaryOperation(let function, let description):
+                    accumulator = (function(accumulator!.currentValue), description(accumulator!.description))
                     resultIsPending = false
-            case .binaryOperation(let function):
-                if accumulator != nil {
-                    
-                     description += symbol
-                    
+            case .binaryOperation(let function, let description):
+                //if accumulator?.currentValue != nil {
                     /*
                      In order to make chain operations happen performPendingBinaryOperations here as well, such as 6 x 5 x 4 x 3, just keeps storing the updated result as an "operand"
                      */
-                    
                     performPendingBinaryOperation()
                     historyStringSuffix = "..."
-                    pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
-                    accumulator = nil
+                    if (accumulator != nil) {
+                        // Only create a pending operation if the accumulator is set, set to nil after so that both the value and description are reset and ready for the next key press
+                        pendingBinaryOperation = PendingBinaryOperation(function: function, description: description, firstOperand: accumulator!)
+                        accumulator = nil
+                    // }
                     resultIsPending = true
                 }
             case .equals:
                 if (resultIsPending) {
-                    if (accumulator != Double.pi) {
-                        description += formatNumber(number: accumulator!)
+                    if (accumulator!.currentValue != Double.pi) {
+                        // description += formatNumber(number: accumulator!)
                     }
                 }
                 historyStringSuffix = " = "
@@ -112,9 +96,8 @@ struct CalculatorBrain {
             case .clear:
                 // Once clear button is clicked clear accumulator, pendingBinaryOpertaions and any history screen 
                 resultIsPending = false
-                accumulator = 0
+                accumulator = (0, " ")
                 pendingBinaryOperation = nil
-                description = " "
                 historyStringSuffix = " "
                 
             case .random:
@@ -122,10 +105,9 @@ struct CalculatorBrain {
                     Use arc4random to generate a number from 0 up to UINT.max then divide by UNINT.max to get a number between 0 and 1, cast both types to get double precision
                 */
                 
-                accumulator = Double(arc4random()) / Double(UINT32_MAX)
+                accumulator!.currentValue = Double(arc4random()) / Double(UINT32_MAX)
             }
         }
-        
     }
     
     /* 
@@ -133,7 +115,7 @@ struct CalculatorBrain {
      */
     
     private mutating func performPendingBinaryOperation() {
-        if pendingBinaryOperation != nil && accumulator != nil {
+        if pendingBinaryOperation != nil && accumulator?.currentValue != nil {
             accumulator = pendingBinaryOperation!.perform(with: accumulator!)
             pendingBinaryOperation = nil
             resultIsPending = false
@@ -144,10 +126,11 @@ struct CalculatorBrain {
     
     private struct PendingBinaryOperation {
         let function: (Double,Double) -> Double
-        let firstOperand: Double
+        let description: (String, String) -> String
+        let firstOperand: (Double, String)
         
-        func perform(with secondOperand: Double) -> Double {
-            return function(firstOperand, secondOperand)
+        func perform(with secondOperand: (Double,String)) -> (Double,String) {
+            return (function(firstOperand.0, secondOperand.0), description(firstOperand.1,secondOperand.1))
         }
     }
     
@@ -156,30 +139,26 @@ struct CalculatorBrain {
     */
     
     mutating func setOperand(_ operand: Double) {
-        accumulator = operand
-        
-        // Only add operand to description if the result is NOT pending a binary Operation
-        
-        if (!resultIsPending) {
-            description = formatNumber(number: operand)
-        }
+        accumulator = (operand, String(operand)) // or \(operand)
     }
     
     var result: Double? {
-        get {
-            return accumulator
+        if accumulator?.currentValue != nil {
+            return accumulator!.currentValue
         }
+        return nil
     }
     
     // Allows us to track the private description string over time, not sure if this is needed or not but does ensure some encapsulation I think
+    // just have to make it work with pendingBinaryOperation Now
     
     var historyString: String {
-        get {
-            return description
+        
+        if pendingBinaryOperation != nil {
+            // description storres the correct description function for the binary symbol, so pass firstOperand and then "" becasue accumulator is nil at this point so we print the pending calculation on screen
+            return (pendingBinaryOperation!.description(pendingBinaryOperation!.firstOperand.1, accumulator?.description ?? ""))
         }
-        set {
-            description = newValue
-        }
+        return accumulator!.description
     }
     
     var suffix: String {
